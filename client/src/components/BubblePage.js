56b48308b42f4f11a04292a93f12b517a6671743
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-05-22 09:32:48 -0700

    login done ... need to finish colorlist and route it privately

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 import { axiosWithAuth } from "../utilities/axiosWithAuth";
@@ -8,7 +6,7 @@ import { axiosWithAuth } from "../utilities/axiosWithAuth";
 const BubblePage = (props) => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property // so props? 
+  // set that data to the colorList state property // so props?; similar to the friends page in friends assignment
 
   useEffect(() => {
     axiosWithAuth().get('/api/colors')
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
+const ColorList = ({ props, colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
@@ -20,7 +20,9 @@ const ColorList = ({ colors, updateColors }) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is is saved right now?// colortoedit? 
+    axiosWithAuth().put(`/api/color/${colorToEdit.id}`, colorToEdit);
+    props.history.push('/bubble-page')
   };
 
   const deleteColor = color => {
